e5c2c16fcd047a512aafc3821afe875a1e76bf0f
JunyoungLee <ljy981123@gmail.com>
2023-08-27 16:45:14 +0900

    fix bear

--- Kirby/Kirby/GameObject/Mob/Bear.cpp
+++ Kirby/Kirby/GameObject/Mob/Bear.cpp
@@ -2,6 +2,7 @@
 #include "Bear.h"
 #include "BoxCollider.h"
 #include "RigidBody2D.h"
+#include "GenPoint.h"
 
 void Bear::Init()
 {
@@ -21,4 +22,37 @@ void Bear::Init()
 	SetOrigin({ 36.0f, 48.0f });
 	boxCol->SetRect({ 0.0f, 0.0f, 24.0f, 24.0f });
 	boxCol->SetOffset({ -12.0f, -24.0f });
+}
+
+void Bear::UpdateMove(float dt)
+{
+	currentEventTime += dt;
+	if (currentEventTime > eventTime && !isRun)
+	{
+		currentEventTime = 3.0f;
+		animator->SetEvent("Rush");
+		isRun = true;
+	}
+	else if (currentEventTime > eventTime && isRun)
+	{
+		currentEventTime = 0.0f;
+		animator->SetEvent("Jump");
+		isRun = false;
+	}
+	SetPosition({ GetPosition().x + (isRun ? 4.0f : 1.0f) * speed * dt , GetPosition().y });
+}
+
+void Bear::Reset()
+{
+	SpriteGO::Reset();
+	update = std::bind(&Bear::UpdateMove, this, std::placeholders::_1);
+	SetPosition(position);
+	currentHP = maxHP;
+
+	currentEventTime = 0.0f;
+	isRun = false;
+
+	outCameraEvent = [this]() {
+		genPoint->MobRemove();
+	};
 }
--- Kirby/Kirby/GameObject/Mob/Bear.h
+++ Kirby/Kirby/GameObject/Mob/Bear.h
@@ -2,10 +2,13 @@
 #include "Mob.h"
 class Bear : public Mob
 {
+    bool isRun = false;
 public:
     Bear(KirbyAbility type = KirbyAbility::None, const std::string textureID = "sprites/mob/Mob_Bear.png", const string& name = "Mob")
         :Mob(type, textureID, name) {}
 
     virtual void Init() override;
+    void UpdateMove(float dt);
+    virtual void Reset() override;
 };
 
